5a0dd3724d1e44ed8627811c23ecdfe78a77fade
Caio Castelhano <castelhanoc@gmail.com>
2025-02-17 19:09:38 -0300

    Adiciona ícones à seção About

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import { LanguageContext } from "../context/LanguageContext";
 import "../styles/About.css";
 import profile from "../images/profile.jpg";
+import { DiReact, DiJavascript1, DiRuby, DiHtml5, DiCss3, DiRor } from "react-icons/di";
 
 export default function About() {
   const { language } = useContext(LanguageContext);
@@ -22,6 +23,23 @@ export default function About() {
               : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."}
           </p>
         </div>
+      </div>
+
+      {/* 🔽 Ícones abaixo da foto e do texto */}
+      <div className="icons-container">
+        {[
+          { icon: <DiHtml5 className="icon" />, label: "HTML5" },
+          { icon: <DiCss3 className="icon" />, label: "CSS3" },
+          { icon: <DiJavascript1 className="icon" />, label: "JavaScript" },
+          { icon: <DiReact className="icon" />, label: "React" },
+          { icon: <DiRuby className="icon" />, label: "Ruby" },
+          { icon: <DiRor className="icon" />, label: "Ruby on Rails" },
+        ].map((item, index) => (
+          <div className="icon-item" key={index}>
+            {item.icon}
+            <p>{item.label}</p>
+          </div>
+        ))}
       </div>
     </section>
   );
