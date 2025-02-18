cff10bc462ec912ccc85fa73ba254dce6a47bd2d
Caio Castelhano <castelhanoc@gmail.com>
2025-02-18 15:55:05 -0300

    Adiciona a seção Projects com os cards já funcionando

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,5 +1,51 @@
+import { useContext } from "react";
+import { LanguageContext } from "../context/LanguageContext";
+import ProjectCard from "./ProjectCard";
+import "../styles/Project.css";
+import cheftoi from "../images/cheftoi.png"
+import giggoers from "../images/giggoers.png"
+
+const projects = [
+  {
+    image: giggoers,
+    title: "GigGoers",
+    description: {
+      PT: "Aplicativo para fãs de música descobrirem e compartilharem eventos em São Paulo.",
+      EN: "An app for music fans to discover and share events in São Paulo."
+    },
+    repoLink: "https://github.com/caiocastelhano/giggoers",
+    liveLink: "https://giggoers-f8d886acbf20.herokuapp.com/"
+  },
+  {
+    image: cheftoi,
+    title: "Chef Toi",
+    description: {
+      PT: "Marketplace que conecta clientes e chefs profissionais para experiências culinárias únicas.",
+      EN: "Marketplace that connects clients and professional chefs for unique culinary experiences."
+    },
+    repoLink: "https://github.com/caiocastelhano/CHEF_TOI",
+    liveLink: "https://cheftoi-ab965da39ea5.herokuapp.com/"
+  }
+];
+
 export default function Project() {
-    return (
-        <h1>Projetos vão estar aqui</h1>
-    ) 
+  const { language } = useContext(LanguageContext);
+
+  return (
+    <section className="projects">
+      <h2>{language === "PT" ? "Projetos" : "Projects"}</h2>
+      <div className="projects-container">
+        {projects.map((project, index) => (
+          <ProjectCard
+            key={index}
+            image={project.image}
+            title={project.title}
+            description={project.description[language]}
+            repoLink={project.repoLink}
+            liveLink={project.liveLink}
+          />
+        ))}
+      </div>
+    </section>
+  );
 }
---
+++ src/components/ProjectCard.jsx
@@ -0,0 +1,33 @@
+import "../styles/ProjectCard.css";
+import PropTypes from "prop-types";
+import { FaGithub, FaExternalLinkAlt } from "react-icons/fa";
+
+export default function ProjectCard({ image, title, description, repoLink, liveLink }) {
+  return (
+    <div className="project-card">
+      <img src={image} alt={title} className="project-image" />
+      <div className="project-info">
+        <h3>{title}</h3>
+        <p>{description}</p>
+        <div className="project-links">
+          <a href={repoLink} target="_blank" rel="noopener noreferrer">
+            <FaGithub /> GitHub
+          </a>
+          {liveLink && (
+            <a href={liveLink} target="_blank" rel="noopener noreferrer">
+              <FaExternalLinkAlt /> Site
+            </a>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+ProjectCard.propTypes = {
+    image: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    repoLink: PropTypes.string.isRequired,
+    liveLink: PropTypes.string.isRequired,
+  };
